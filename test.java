aa52eed21cf002dca36695653a1a6c9e1d6ad71b
zyte09 <allen.aranez32@gmail.com>
2024-03-10 16:49:42 +0800

    first commit

---
+++ test.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+public class test { 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame(); //create a new frame
+        frame.setTitle("JFrame title goes here"); //set the title
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //set the exit option
+        frame.setResizable(false); //prevent the window from resizing
+        frame.setSize(420, 420); //set x, y, width and height
+        frame.setVisible(true); //show the frame
+
+        ImageIcon image = new ImageIcon("pochi.jpg"); //create an ImageIcon
+        frame.setIconImage(image.getImage()); //change the icon
+        frame.getContentPane().setBackground(new Color(0, 255, 255)); //change bg color or //(Color.green)
+    }
+}   
